ab755ebd927f7e17181cf0bb1aa0c0d98f6c6d25
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-02-21 09:13:56 +0530

    Add get-users feature

---
+++ user-service/src/main/java/com/karthik/userservice/service/UserService.java
@@ -0,0 +1,21 @@
+package com.karthik.userservice.service;
+
+
+import com.karthik.userservice.dao.UserRepository;
+import com.karthik.userservice.domain.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService
+{
+
+    private final UserRepository userRepository;
+    public List<Users> getAllUsers()
+    {
+        return userRepository.findAll();
+    }
+}
